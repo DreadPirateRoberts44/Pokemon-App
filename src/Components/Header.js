9be70f1019598f0e07c7adc932a7f4a281f54da6
Mike Yamo <yamokoski.993@osu.edu>
2022-08-05 23:37:58 -0400

    project start

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src="ariados.ico"
+          alt="Oakley the Ariados"
+          width="60"
+          height="60"
+        ></img>
+        Trainer's Web
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
